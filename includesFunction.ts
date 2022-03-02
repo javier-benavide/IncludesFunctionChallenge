91b361f7062ca23f7c1a86d69a106df4e752c235
Javier Benavides <javier@neatpagos.com>
2022-03-02 11:02:08 -0300

    Initial version of the includesFunction Challenge with tests

---
+++ includesFunction.ts
@@ -0,0 +1,37 @@
+/*
+EXERCISE: Includes Function
+
+Exercise description:
+
+You need to replicate the "string".includes() native function for strings
+
+So arrayIncludes(string, seachedText) === array.includes(value)
+
+But you can not use any .indexOf native function or the .includes
+or regex in your code
+
+Also at the end of the arrayIncludes()'s code you need to log how many
+loops and recursion depth was used
+
+includes description:
+
+Returns true if searchString appears as a substring of the result of converting
+this object to a String, at one or more positions that are greater than or equal
+to position; otherwise, returns false.
+
+Testing:
+
+You must know that when you run the testing (yarn test), the first 3 test
+shows the input and the expected result. The other test have hidden values
+that you will never know until the end of the interview.
+*/
+
+/**
+ * @param {string}  parentText.
+ * @param {string} searchedText.
+ * @returns {boolean} True if searchedText is in originalText
+ */
+export function arrayIncludes(parentText: string, searchedText: string): boolean{
+    //Write your code here
+    return false;
+}
